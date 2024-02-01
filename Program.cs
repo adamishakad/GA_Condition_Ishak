7bebd578f20a9efacc6792f3580fe47a246093fd
adamishakad <155333634+adamishakad@users.noreply.github.com>
2024-02-01 14:00:02 -0800

    Updated Program File

--- Program.cs
+++ Program.cs
@@ -1,10 +1,52 @@
-﻿namespace GA_Condition_Ishak
+﻿// Ishak
+using System.ComponentModel.Design;
+
+namespace GA_Condition_Ishak
 {
     internal class Program
     {
         static void Main(string[] args)
+
         {
-            Console.WriteLine("You are an adult!");
+            //Console.WriteLine("Checking conditions!");
+
+            //int number = 6;
+
+            //if (number == 5)
+            //{
+            //    Console.WriteLine("Number is equal to 5");
+            //}
+            //else if (number > 5)
+
+            //{
+            //    Console.WriteLine("Numbe is greater than 5");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Number is less than 5");
+            //}
+            int temperature = 75;
+            string weatherInfo = "";
+
+            if (temperature > 180) 
+            {
+                weatherInfo = "It's a hot day!";
+            }
+            else if (temperature > 60) 
+            {
+                weatherInfo = "It's a pleasant day.";
+            }
+            else
+            {
+                weatherInfo = "It's a bit chilly.";
+            }
+
+            Console.WriteLine(weatherInfo);
+                
+            
+
+
         }
+
     }
 }
